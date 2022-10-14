80f88cec11dedbb9fc4d976ded6312cbed0c684a
martin2176 <martin2176@yahoo.com>
2022-10-14 06:28:25 -0400

    Update BackendController.java

--- src/main/java/com/martin/backend/controllers/BackendController.java
+++ src/main/java/com/martin/backend/controllers/BackendController.java
@@ -64,7 +64,7 @@ public class BackendController {
             .withTag(DDTags.RESOURCE_NAME, "web.request")
             .start();
 		try (Scope scope = tracer.activateSpan(span)) {
-            span.setTag("type", "Web");
+            span.setTag("type", "web");
             span.setTag("owner", "MartinFK");
 		ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, null,
 				new ParameterizedTypeReference<String>() {
